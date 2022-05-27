4f1230a9f794176155ebbdecaf5cb1706790fad0
Eloy Pérez <eperez.otero@atsistemas.com>
2022-05-27 10:41:11 +0200

    feat(layout): adding basic switch theme feature

--- src/app/layout/layout.js
+++ src/app/layout/layout.js
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import { ThemesConfig } from '../../assets/themes/themes.config';
 
 export function Layout() {
+  const [theme, setTheme] = useState(ThemesConfig.themes.Light);
+
+  function switchTheme() {
+    theme === ThemesConfig.themes.Light ? setTheme(ThemesConfig.themes.Dark) : setTheme(ThemesConfig.themes.Light);
+    document.getElementById('layout').className = theme;
+  }
+
   return (
-    <div className="container-fluid h-100">
-      <div className="row h-100">
-        <div className="col-12">
-          <Outlet />
+    <div id="layout" className="light-theme">
+      <div className="container-fluid h-100">
+        <div className="row h-100">
+          <div className="col-12">
+            <button onClick={switchTheme}>Theme</button>
+            <Outlet />
+          </div>
         </div>
       </div>
     </div>
---
+++ src/assets/themes/themes.config.js
@@ -0,0 +1,6 @@
+export const ThemesConfig = {
+  themes: {
+    Light: 'light-theme',
+    Dark: 'dark-theme'
+  }
+};
