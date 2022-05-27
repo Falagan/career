d5e76ceb940053ba949dcfef19945010bfcbbf89
Eloy Pérez <eperez.otero@atsistemas.com>
2022-05-27 14:17:29 +0200

    chore: setting up config by envs

--- src/app/layout/layout.js
+++ src/app/layout/layout.js
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { ThemesConfig } from '../../assets/themes/themes.config';
+import { ENVIRONMENT } from '../../config';
 
 export default function Layout() {
   const [theme, setTheme] = useState(ThemesConfig.themes.Light);
+  const title = ENVIRONMENT.TITLE;
 
   function switchTheme() {
     theme === ThemesConfig.themes.Light ? setTheme(ThemesConfig.themes.Dark) : setTheme(ThemesConfig.themes.Light);
@@ -15,6 +17,7 @@ export default function Layout() {
       <div className="container-fluid h-100">
         <div className="row h-100">
           <div className="col-12">
+            <h1>{title}</h1>
             <button onClick={switchTheme}>Theme</button>
             <Outlet />
           </div>
