1a70ca8e3f0d5888d3d82a4abbb4ab850d62228e
Eloy Pérez <eperez.otero@atsistemas.com>
2022-06-02 15:47:48 +0200

    feat: basic main components definition

--- src/app/components/projects/projects.js
+++ src/app/components/projects/projects.js
@@ -1,3 +1,24 @@
+import { useQuery } from 'react-query';
+import { GitHubRepository } from '../../repositories/github-repository';
+
 export default function Projects() {
-  return <div>Projects!</div>;
+  const { isLoading, isError, data, error } = useQuery('gitHubRepositories', GitHubRepository.getRepositories);
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+  return (
+    <div>
+      Projects!
+      <ul>
+        {data.map((repo) => (
+          <li key={repo.name}>{repo.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
