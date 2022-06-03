1db433f0340059b55022cbb3566fc9c233027ee5
Eloy Pérez <eperez.otero@atsistemas.com>
2022-06-03 13:07:53 +0200

    feat: multi-language support

--- src/app/components/projects/projects.js
+++ src/app/components/projects/projects.js
@@ -1,8 +1,10 @@
+import { useTranslation } from 'react-i18next';
 import { useQuery } from 'react-query';
 import { GitHubRepository } from '../../repositories/github-repository';
 
 export default function Projects() {
   const { isLoading, isError, data, error } = useQuery('gitHubRepositories', GitHubRepository.getRepositories);
+  const { t } = useTranslation();
 
   if (isLoading) {
     return <span>Loading...</span>;
@@ -14,6 +16,7 @@ export default function Projects() {
   return (
     <div>
       Projects!
+      <h2>{t('welcome')}</h2>
       <ul>
         {data.map((repo) => (
           <li key={repo.name}>{repo.name}</li>
